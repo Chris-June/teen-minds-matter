6cfd908f17d840e5df863ed5f047a463d7de5727
chris-june <chris.june@intellisync.ca>
2024-12-01 13:27:38 -0500

    fix: adjust top padding in ArticlesPage to prevent header overlap

--- src/pages/articles/ArticlesPage.tsx
+++ src/pages/articles/ArticlesPage.tsx
@@ -29,7 +29,7 @@ export function ArticlesPage() {
   };
 
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="container mx-auto px-4 pt-20 pb-8">
       <motion.div
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
